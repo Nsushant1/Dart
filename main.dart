be2bccf568d0c2941da482b1250f79bdc65e0670
Sushant Neupane <nsushaant72@gmail.com>
2025-04-14 14:38:57 +0545

    learned about operators and variables

---
+++ main.dart
@@ -0,0 +1,15 @@
+// First Dart Program
+
+void main(){
+	print("Hello");
+}
+
+//Print the value of 1 over the square root of 2 . Confirm that it equals the sine of 45° .
+import "dart:math";
+void main (){
+  print (1/sqrt(2));
+  
+  print (sin(45));
+  
+  print( (1/sqrt(2))==(sin(45)));
+}
