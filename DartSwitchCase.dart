316d4051881d36d61a65423a424a0f7306f1c8c6
Sushant Neupane <nsushaant72@gmail.com>
2025-07-21 07:38:59 +0545

    dart questions practice

---
+++ DartSwitchCase.dart
@@ -0,0 +1,24 @@
+void main() {
+  String grade = 'A';
+
+  switch (grade) {
+    case 'A':
+      print("Excellent result");
+      break;
+
+    case 'B':
+      print("Average result");
+      break;
+
+    case 'C':
+      print("Poor result");
+      break;
+
+    case 'D':
+      print("FAILED!");
+      break;
+
+    default:
+      print("Invalid Input");
+  }
+}
