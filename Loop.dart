f28d4507416134b779968e8745ea8d0b0b6c533b
Sushant Neupane <nsushaant72@gmail.com>
2025-07-24 07:07:03 +0545

    dart questions

---
+++ Loop.dart
@@ -0,0 +1,22 @@
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    print("Sushant Neupane ");
+  }
+
+  for (int i = 100; i >= 1; i) {
+    print("HelloWorld");
+    print(i);
+  }
+
+  int j = 1;
+  while (j <= 5) {
+    print(j);
+    j++;
+  }
+
+  int k = 1;
+  do {
+    print(k);
+    k++;
+  } while (k <= 5);
+}
