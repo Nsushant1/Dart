c8a744f9481142aba134daffa101b3625ff759f8
Sushant Neupane <nsushaant72@gmail.com>
2025-07-26 18:59:30 +0545

    more dart questions

---
+++ Password.length.dart
@@ -0,0 +1,8 @@
+void main() {
+  String password = "sus";
+  if (password.length >= 8) {
+    print("The password is valid");
+  } else {
+    print("The password is invalid");
+  }
+}
---
+++ PositionalParameter.dart
@@ -0,0 +1,9 @@
+void main() {
+  printName("Ram", 14, "Ktm");
+}
+
+void printName(String name, int age, String address) {
+  print(name);
+  print(age);
+  print(address);
+}
