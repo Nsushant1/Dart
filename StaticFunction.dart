34b034f2926ce3e2f46e231a91cb46090fd4872c
Sushant Neupane <nsushaant72@gmail.com>
2025-08-01 13:25:57 +0545

    question coming to end

---
+++ StaticFunction.dart
@@ -0,0 +1,12 @@
+void main() {
+  print(Constants.printValue());
+}
+
+class Constants {
+  static String name = "Sushant";
+  static String address = "Jhapa";
+
+  static int printValue() {
+    return 10;
+  }
+}
